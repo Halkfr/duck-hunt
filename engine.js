4aef292124ef85ac403ad4e5c561cb536e30c2d6
maggieeagle <oryol.margo@gmail.com>
2023-07-14 10:46:34 +0300

    add manage level function (need to stop ducks when the game starts)

--- engine.js
+++ engine.js
@@ -28,11 +28,11 @@ fillInterfaceElements()
 const countProps = () => {
     // background.png size 1265x769
     const kWidth = window.innerWidth / 1265, kHeight = window.innerHeight / 769, k = kHeight < kWidth ? kHeight : kWidth
-    const duckLeft = parseInt(window.innerWidth) * 0.48 + 'px'
+    const duckLeft = Math.round(parseInt(window.innerWidth) * 0.48) + 'px'
     const duckTop = Math.round(window.innerHeight / 2 + (769 * k) * 0.25) + 'px'
     const duckWidth = Math.round(k * 70) + 'px'
     const duckMoveArea = { height: Math.round(window.innerHeight * 0.5), width: Math.round(window.innerWidth * 0.95) }
-    const hunterLeft = parseInt(window.innerWidth) / 3 + 'px'
+    const hunterLeft = Math.round(parseInt(window.innerWidth) / 3) + 'px'
     const hunterTop = Math.round(window.innerHeight / 2 + (769 * k) * 0.1) + 'px'
     const hunterWidth = Math.round(k * 150) + 'px'
     const hunterMoveArea = { leftX: Math.round(window.innerWidth * 0.0005), rightX: Math.round(window.innerWidth * 0.84) }
@@ -42,21 +42,71 @@ const countProps = () => {
 import { Duck } from '/actors.js'
 import { Hunter } from '/actors.js'
 
-function startLevel() {
-    let props = countProps()
-    console.log(props.duck.top)
-    const duck1 = new Duck('duck/', 'duck-left-up.gif', 7, props.duck.left, props.duck.top, props.duck.width, props.duck.moveArea)
-    const duck2 = new Duck('duck-brown/', 'duck-left-up.gif', 7, props.duck.left, props.duck.top, props.duck.width, props.duck.moveArea)
-    const hunter = new Hunter('hunter-right.png', 15, props.hunter.left, props.hunter.top, props.hunter.width, props.hunter.moveArea)
+export let actors = []
 
-    return [duck1, duck2, hunter]
+async function manageLevel() {
+    let ducksReleased = 0, ducksBatchSize = 0
+
+    while (ducksReleased < 10) {
+        if (document.querySelectorAll('#duck').length == 0) {
+            ducksBatchSize = getRandomInt(3) + 1
+
+            console.log('ducks batch', ducksBatchSize)
+
+            if (ducksBatchSize <= 10 - ducksReleased) {
+                createDucksBanch(ducksBatchSize)
+            }
+            else {
+                createDucksBanch(10 - ducksReleased)
+            }
+        }
+
+        const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+        await sleep(1000)
+    }
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    function createDucksBanch(n) {
+
+        const props = countProps()
+
+        const createGreenDuck = () => {
+            return new Duck('duck/', 'duck-left-up.gif', 7, props.duck.left, props.duck.top, props.duck.width, props.duck.moveArea)
+        }
+
+        const createBrownDuck = () => {
+            return new Duck('duck-brown/', 'duck-left-up.gif', 7, props.duck.left, props.duck.top, props.duck.width, props.duck.moveArea)
+        }
+
+        const createHunter = () => {
+            return new Hunter('hunter-right.png', 15, props.hunter.left, props.hunter.top, props.hunter.width, props.hunter.moveArea)
+        }
+
+        let isBrownDuck = getRandomInt(2)
+
+        if (isBrownDuck == 1) {
+            actors.push(createBrownDuck())
+        }
+
+        for (let i = 0; i < n - isBrownDuck; i++) {
+            actors.push(createGreenDuck())
+        }
+
+        ducksReleased += n
+    }
 }
 
-export let actors = startLevel()
+setTimeout(manageLevel, 1000)
+
+console.log(actors)
 
 document.getElementById("grass").addEventListener("click", bulletsLeft);
-document.getElementById("hunter-hitbox").addEventListener("click", bulletsLeft);
-document.getElementById("duck-hitbox").addEventListener("click", bulletsLeft);
+document.querySelectorAll(".hitbox").forEach(hitbox => {
+    addEventListener("click", bulletsLeft);
+});
 
 function bulletsLeft() {
     let bulletsLeftElement = document.getElementById("bullets-left");
@@ -75,7 +125,13 @@ function bulletsLeft() {
 
 onresize = () => {
     // console.log('resize')
-    props = countProps()
-    duck.resize(props.duck.moveArea, props.duck.width)
-    hunter.resize(props.hunter.moveArea, props.hunter.width, props.hunter.top)
+    let props = countProps()
+    console.log(props)
+    for (let i = 0; i < actors.length; i++) {
+        if (actors[i].constructor == "Duck") {
+            actors[i].resize(props.duck.moveArea, props.duck.width, props.duck.top)
+        } else {
+            actors[i].resize(props.hunter.moveArea, props.hunter.width, props.hunter.top)
+        }
+    }
 };
